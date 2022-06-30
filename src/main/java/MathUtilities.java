f42b2ccfa90cd6de2469163911e4a16f27e2ff5c
Ryan <ryan@Zipcoders-MacBook-Pro-55.local>
2022-06-30 14:18:27 -0400

    math util quiz review

--- src/main/java/MathUtilities.java
+++ src/main/java/MathUtilities.java
@@ -37,12 +37,8 @@ public class MathUtilities{
    * @return true if the number is odd, false if it is even
    */
   public Boolean isOdd(Integer number){
-      if (number % 2 != 0) {
-          return true;
-      }
-      return false;
+      return number % 2 != 0;
   }
-
 
   /**
    * Multiply the number by itself
